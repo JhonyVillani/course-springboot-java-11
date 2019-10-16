ed8b4ddfdd6f13803880afaa8b402999d5461de8
Jhony <jhony.v.m@hotmail.com>
2019-10-16 12:02:28 -0300

    Service layer, component registration

---
+++ src/main/java/com/jhonyvillani/course/services/UserService.java
@@ -0,0 +1,26 @@
+package com.jhonyvillani.course.services;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.jhonyvillani.course.entities.User;
+import com.jhonyvillani.course.repositories.UserRepository;
+
+@Service
+public class UserService {
+	
+	@Autowired
+	private UserRepository repository;
+	
+	public List<User> findAll(){
+		return repository.findAll();
+	}
+	
+	public User findById(Long id) {
+		Optional<User> obj = repository.findById(id);
+		return obj.get();
+	}
+}
